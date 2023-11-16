2c7e26c4e8c720fddbf4e5ffc77ed414342fe408
jonasmoons <moons_jonas@outlook.com>
2023-11-16 12:13:31 +0100

    added rules to the models

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "lists#index"
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  post "lists", to: "lists#create"
+  get "lists/:id", to: "lists#show", as: :list
+  patch "movies/:id/bookmark", to: "movies#bookmark", as: :bookmark_movie
+  delete "movies/:id/bookmark", to: "movies#unbookmark", as: :unbookmark_movie
 end
